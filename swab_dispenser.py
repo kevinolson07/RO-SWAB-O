44afe8fa88cad43b7a5513d12aea60d6547e5289
kevinolson07 <57046983+kevinolson07@users.noreply.github.com>
2021-04-06 14:04:24 -0700

    added functions for control of motor and ultrasonic sensor

--- swab_dispenser.py
+++ swab_dispenser.py
@@ -1,41 +1,64 @@
 import RPi.GPIO as GPIO
 import time
 
+
+
+#------------
+# Functions
+#------------
+
+
+# Nema 17 motor control
+def nema_motor():
+    for i in range(revolutions):
+        for x in range(stepsPerRev):
+            GPIO.output(motor1, GPIO.HIGH)
+            time.sleep(.001)
+            GPIO.output(motor1, GPIO.LOW)
+            time.sleep(.001)
+
+# 28BYJ-48 steper motor control
+def small_motor():
+    print("some code")
+
+# ultrasonic sensor measurement
+def measure():
+    GPIO.output(GPIO_trigger, GPIO.LOW)
+    time.sleep(.000002)
+    GPIO.output(GPIO_trigger, GPIO.HIGH)
+    time.sleep(.000005)
+    GPIO.setup(GPIO_trigger, GPIO.IN)
+
+    
+    while GPIO.input(GPIO_echo) == 0:
+        start = time.time()
+
+    while GPIO.input(GPIO_echo) == 1:
+        stop = time.time()
+
+    elapsed_time = stop - start
+    distance = (elapsed * 34300)/2
+    return distance
+
+
+    
+#-----------
+# Main
+#-----------
 revolutions = 1
 stepsPerRev = 200
 # pin used for ultrasonic signal
-ping = 11
+GPIO_trigger = 11
 # pins used to control motor
 motor_dir1 = 35
 motor1 = 36
 motor_dir2 = 37
 motor2 = 38
-pins = [ping, motor1, motor_dir1, motor2, motor_dir2]
+pins = [GPIO_trigger, motor1, motor_dir1, motor2, motor_dir2]
 # setting the pin nomenclature to match the Board pinouts
 GPIO.setmode(GPIO.BOARD)    
 GPIO.setup(pins,GPIO.OUT)
-
-GPIO.output(ping, GPIO.LOW)
-time.sleep(.000002)
-GPIO.output(ping, GPIO.HIGH)
-time.sleep(.000005)
-GPIO.output(ping GPIO.LOW)
-
-while GPIO.input(ping) == LOW:
-    start_time = time.time()
-
-while GPIO.input == HIGH:
-    end_time = time.time()
-
-duration = end_time - start_time
-distance = duration * (34000/2)
-print (distance, "cm")
-time.sleep(0.5)
-
 GPIO.output(motor_dir1, GPIO.LOW)
-for i in range(revolutions):
-    for x in range(stepsPerRev):
-        GPIO.output(motor1, GPIO.HIGH)
-        time.sleep(.001)
-        GPIO.output(motor1, GPIO.LOW)
-        time.sleep(.001)
+
+measure():
+
