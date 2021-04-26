c0d00d831c5cc7ed225766ca6451ff23c3ec1b49
kevinolson07 <kevin.olson07@gmail.com>
2021-04-26 16:13:03 -0700

    added function for ultrasonic sensor

--- swab_dispenser.py
+++ swab_dispenser.py
@@ -23,20 +23,33 @@ def small_motor():
     print("some code")
 
 # ultrasonic sensor measurement
+
 def measure():
-    GPIO.setup(GPIO_trigger, GPIO.OUT)
-    GPIO.output(GPIO_trigger, GPIO.LOW)
-    time.sleep(.000002)
-    GPIO.output(GPIO_trigger, GPIO.HIGH)
-    time.sleep(.000005)
-    GPIO.setup(GPIO_trigger, GPIO.IN)
+    print("distance measuremnet in progress")
+    GPIO.output(GPIO_trigger, False)
+    print("waiting forsensor to settle")
+    time.sleep(1)
+    GPIO.output(GPIO_trigger, True)
+    time.sleep(.00001)
+    GPIO.output(GPIO_trigger, False)
+    while GPIO.input(GPIO_echo) == 0:
+        start = time.time()
+    while GPIO.input(GPIO_echo) == 1:
+        stop = time.time()
+
+#     GPIO.setup(GPIO_trigger, GPIO.OUT)
+#     GPIO.output(GPIO_trigger, GPIO.LOW)
+#     time.sleep(.000002)
+#     GPIO.output(GPIO_trigger, GPIO.HIGH)
+#     time.sleep(.000005)
+#     GPIO.setup(GPIO_trigger, GPIO.IN)
 
     
-    while GPIO.input(GPIO_trigger) == 0:
-        start = time.time()
+#     while GPIO.input(GPIO_trigger) == 0:
+#         start = time.time()
 
-    while GPIO.input(GPIO_trigger) == 1:
-        stop = time.time()
+#     while GPIO.input(GPIO_trigger) == 1:
+#         stop = time.time()
 
     elapsed_time = stop - start
     distance = (elapsed_time * 34000)/2
@@ -50,7 +63,8 @@ def measure():
 revolutions = 1
 stepsPerRev = 25
 # pin used for ultrasonic signal
-GPIO_trigger = 11
+GPIO_trigger = 16
+GPIO_echo = 18
 # pins used to control motor
 motor_dir1 = 35
 motor1 = 36
@@ -60,10 +74,14 @@ pins = [GPIO_trigger, motor1, motor_dir1, motor2, motor_dir2]
 # setting the pin nomenclature to match the Board pinouts
 GPIO.setmode(GPIO.BOARD)    
 GPIO.setup(pins,GPIO.OUT)
+GPIO.setup(GPIO_echo,GPIO.IN)
 GPIO.output(motor_dir1, GPIO.LOW)
 
 while 1:
     distance = measure()
-    print(distance)
+    print(distance, "cm")
     time.sleep(.5)
+    if distance >= 6:
+        nema_motor()
+        print("nema motor function ran!!!!!!!")
 
